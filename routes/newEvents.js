0eb03f4a91e7412fadcfad51a13705b798220beb
HasanKeyf <donmez81@gmail.com>
2015-03-01 00:34:45 -0500

    routes fixes

--- routes/newEvents.js
+++ routes/newEvents.js
@@ -2,8 +2,9 @@
 var express = require('express');
 var router = express.Router();
 var io = require('../socket');
-/* GET home page. */
-router.get('/newEvent', function(req, res, next) {
+/* GET new event. */
+
+router.get('/', function(req, res, next) {
     console.log('in new event');
     io.sendNewEvent(req.query.title,null);
 
@@ -11,13 +12,8 @@ router.get('/newEvent', function(req, res, next) {
     res.status(200).send('Message Sent!');
 });
 
-router.post('/newEvent', function(req, res, next) {
-    console.log('in new event');
-    io.sendNewEvent(req.body,null);
 
-    console.log('New event!!!');
-    res.status(200).send('Message Sent!');
-});
+
 
 
 module.exports = router;
