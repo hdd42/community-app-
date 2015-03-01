386489d8eb5efb7bb8a0cb78d828d19a42afd197
HasanKeyf <donmez81@gmail.com>
2015-03-01 00:25:18 -0500

    routes added

--- routes/newEvents.js
+++ routes/newEvents.js
@@ -11,6 +11,15 @@ router.get('/newEvent', function(req, res, next) {
     res.status(200).send('Message Sent!');
 });
 
+router.post('/newEvent', function(req, res, next) {
+    console.log('in new event');
+    io.sendNewEvent(req.body,null);
+
+    console.log('New event!!!');
+    res.status(200).send('Message Sent!');
+});
+
+
 module.exports = router;
 
 
