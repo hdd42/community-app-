0d4192f5008d8741940bdf9f6b6fb1ace89b8c2f
HasanKeyf <donmez81@gmail.com>
2015-03-02 15:48:25 -0500

    index view for entry

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- // res.render('index', { title: 'Express' });
-res.send('index.html');
+  res.render('index', { title: 'Community App' });
+//res.send('index.html');
 });
 
 module.exports = router;
